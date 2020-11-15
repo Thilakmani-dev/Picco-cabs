18f50fca7df0fe80bd1e5a531b4b9eaca8018f28
Thilakmani-works <thilakmani777@gmail.com>
2020-11-15 14:09:09 +0530

    content added

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-function counter(){
-  document.querySelector('.counting').each(function() {
-    var $this = document.querySelector(this),
-        countTo = $this.attr('data-count');
-    
-    document.querySelector({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
-  
-    {
-  
-      duration: 3000,
-      easing:'linear',
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum);
-        //alert('finished');
+const counters = document.querySelectorAll(".counting");
+const speed =100;
+
+function startCounter() {
+   counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-count')
+      const count = +counter.innerText;
+
+      const inc = target / speed;
+      console.log(inc)
+      if (count < target) {
+        counter.innerText = count + inc;
+        setTimeout(updateCount, 25);
+      } else {
+        count.innerText = target;
       }
-  
-    });  
-    
-  
-  });
+
+    }
+    updateCount();
+  })
 }
+
+  window.onscroll = function () { myFunction() };
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
